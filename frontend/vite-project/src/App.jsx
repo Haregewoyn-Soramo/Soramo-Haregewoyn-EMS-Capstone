a1da987bb467211f5c3b91e5d4f55f481cbb6f15
haregewoyn-soramo <haregewoynkeno@gmail.com>
2024-06-04 16:22:32 -0600

    capstone project

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,13 +1,34 @@
-import './App.css'
+import { createTheme } from '@mui/material/styles';
+import './App.css';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import {themeSettings} from './state/theme';
+import { useSelector } from 'react-redux';
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom'
+import { useMemo } from 'react';
+import Dashboard from './scenes/Dashboard'
+import Layout from './scenes/Layout'
 
 
 
 function App() {
-  
+  const mode = useSelector((state)=>state.global.mode);
+  const theme = useMemo(()=>createTheme(themeSettings(mode)),[mode]);
+
   return (
-    <>
+   <div className='app'>
+    <BrowserRouter>
+    <ThemeProvider theme={ theme}>
+         <CssBaseline/>
+         <Routes>
+          <Route element = {<Layout />}>
+            <Route path= '/' element = {<Navigate to = '/dashboard' replace />} />
+            <Route path= '/dashboard' element = {< Dashboard />} />
+           </Route>
+         </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
       
-    </>
+   </div>
   )
 }
 
